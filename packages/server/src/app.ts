bda2390292a8a723b7d7f0f61a1c9d62309c8382
spenceradolph <spenceradolph@ymail.com>
2022-03-02 19:14:12 +0000

    removed api call for now

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -3,7 +3,3 @@ import express from 'express';
 export const app = express();
 
 app.use('/', express.static(`${__dirname}/build`)); // Expected to be put there by the root build script
-
-app.get('/api', (req, res) => {
-	res.send('hello from server api...');
-});
